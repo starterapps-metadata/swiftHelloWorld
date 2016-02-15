ad7097162fefcdc158ed51771d60f479141ec25e
rolivieri <roliv@us.ibm.com>
2016-02-15 09:53:06 -0600

    Updates to starter app

--- app/Sources/Server/main.swift
+++ app/Sources/Server/main.swift
@@ -1,12 +1,24 @@
 /**
-* main.swift
-*
+ * Copyright IBM Corporation 2016
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+/**
 * Creates a simple HTTP server that listens for incoming connections on port 9080.
 * For each request receieved, the server simply sends a simple hello world message
 * back to the client.
-*
-* Copyright © 2016 IBM. All rights reserved.
-*/
+**/
 
 #if os(Linux)
 import Glibc
